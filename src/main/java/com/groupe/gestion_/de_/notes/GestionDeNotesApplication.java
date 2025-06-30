7214cbeb6a54eae956232ac86f9c0412ed8f0139
ngaland <ngaland093@gmail.com>
2025-06-30 18:47:05 +0100

    premier commit

---
+++ src/main/java/com/groupe/gestion_/de_/notes/GestionDeNotesApplication.java
@@ -0,0 +1,14 @@
+package com.groupe.gestion_.de_.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeNotesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GestionDeNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupe/gestion_/de_/notes/GestionDeNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupe.gestion_.de_.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
