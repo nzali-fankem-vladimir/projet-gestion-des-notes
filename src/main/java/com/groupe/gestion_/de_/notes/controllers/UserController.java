f88cfd395de9959fea8c3cca375ee07e600c70b1
Gounde Talla Wafo Joseph <djopiano@gmail.com>
2025-07-21 06:15:09 +0100

    user registration and search method

--- src/main/java/com/groupe/gestion_/de_/notes/controllers/UserController.java
+++ src/main/java/com/groupe/gestion_/de_/notes/controllers/UserController.java
@@ -8,9 +8,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/admin/users")
 @Tag(name = "Grade Management API", description = " application endpoints !")
 public class UserController {
 
@@ -22,6 +26,7 @@ public class UserController {
 
     @Operation ( summary = "endpoint for registering a new studentuser!")
     @PostMapping("/students") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can register a student
     public ResponseEntity<UserResponse> registerStudent(@Valid @RequestBody StudentRequest request) {
         UserResponse response = userService.registerStudent(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -29,9 +34,74 @@ public class UserController {
 
     @Operation ( summary = "endpoint for registering a new teacheruser!")
     @PostMapping("/teachers") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can register a teacher
     public ResponseEntity<UserResponse> registerTeacher(@Valid @RequestBody TeacherRequest request) {
         UserResponse response = userService.registerTeacher(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @Operation ( summary = "endpoint to perform an Id search!")
+    @GetMapping("/{id}") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can make researches on users
+    public ResponseEntity<Optional<UserResponse>> findById(@Valid @PathVariable Long id){
+        Optional<UserResponse> optionalresponse = userService.findById(id);
+        if(optionalresponse.isPresent()){
+            optionalresponse.get();
+            return new ResponseEntity<>(optionalresponse, HttpStatus.FOUND);
+        }else {
+            return new ResponseEntity<>(optionalresponse,HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation ( summary = "endpoint to perform a search base on Username!")
+    @GetMapping("/username/{username}") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can make researches on users
+    public ResponseEntity<Optional<UserResponse>> findByUsername(@Valid @PathVariable String username){
+        Optional<UserResponse> optionalresponse = userService.findByUsername(username);
+        if(optionalresponse.isPresent()){
+            optionalresponse.get();
+            return new ResponseEntity<>(optionalresponse, HttpStatus.FOUND);
+        }else {
+            return new ResponseEntity<>(optionalresponse, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation ( summary = "endpoint to perform a search base on Email!")
+    @GetMapping("/email/{email}") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can make researches on users
+    public ResponseEntity<Optional<UserResponse>> findByEmail(@Valid @PathVariable String email){
+        Optional<UserResponse> optionalresponse = userService.findByEmail(email);
+        if(optionalresponse.isPresent()){
+            optionalresponse.get();
+            return new ResponseEntity<>(optionalresponse,HttpStatus.FOUND);
+        }else{
+            return new ResponseEntity<>(optionalresponse, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Operation ( summary = "endpoint to get all users!")
+    @GetMapping("/users") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can make researches on users
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+        List<UserResponse> listuser = userService.getAllUsers();
+        return new ResponseEntity<>(listuser, HttpStatus.FOUND);
+    }
+
+
+    @Operation ( summary = "endpoint to perform an update on user data!")
+    @PutMapping("/update/{id}") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can update user's information
+    public ResponseEntity<UserResponse> updateUser(@Valid @PathVariable Long id, @RequestBody UserRequest request){
+        UserResponse userupdated = userService.updateUser(id, request);
+        return new ResponseEntity<>(userupdated, HttpStatus.OK);
+    }
+
+    @Operation ( summary = "endpoint to perform a delete!")
+    @DeleteMapping("/delete/{id}") // Simpler URL
+    @PreAuthorize("hasRole('ADMIN')") // ONLY ADMIN can make researches on users
+    public ResponseEntity<Void> deleteUserById(@Valid @PathVariable Long id){
+        userService.deleteUserById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
